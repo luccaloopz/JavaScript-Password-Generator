f39d4c52c8cda0781a4ff01eb362350e16e22256
luccaloopz <luccamartins2012@gmail.com>
2022-03-30 20:17:34 -0700

    functions have been established. Now need to make sure the instructions within them make sense

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,6 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const answeryes = "yes"
+const answerno = "no"
+
+var yesLC = "yesincludeLC"
+var noLC = "noincludeLC"
+
+var yesUC = "yesincludeUC"
+var noUC = "noincludeUC"
+
+var yesN = "yesincludeN"
+var noN = "noincludeN"
+
+var yesS = "yesincludeS"
+var noS = "noincludeS"
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -15,15 +30,15 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
   alert("You will now begin choosing your password criteria.")
   issuePrompts();
-  if (issuePrompts()) {
-    "then it generates a password"
-  }
+  // if (issuePrompts()) {
+  //   "then it generates a password"
+  // }
 }
 
 function issuePrompts() {
   characterLength();
   characterType();
-  validateUserChoice();
+  // validateUserChoice();
 }
 
 function characterLength() {
@@ -32,57 +47,87 @@ function characterLength() {
     return passwordLength
   }
   else {
-    return "Passwords must be at least 8 characters in length."
+    alert("Passwords must be at least 8 characters and no more than 128 characters.")
+    characterLength();
   }
-}
+};
 
 function characterType() {
-  confirmLowercase();
-  confirmUppercase();
-  confirmNumeric();
-  confirmSpecialCharacters();
-}
+  var resultLC = confirmLowercase();
+  if (resultLC === answeryes) { 
+    return yesLC;
+  }
+  else if (resultLC === answerno) {
+    return noLC;
+    resultUC
+  };
+  var resultUC = confirmUppercase();
+  if (resultUC === answeryes) {
+    return yesUC
+  }
+  else if (resultUC === answerno) {
+    return noUC
+  };
+  var resultN = confirmNumeric();
+  if (resultN === answeryes) {
+    return yesN
+  }
+  else if (resultN === answerno) {
+    return noN
+  };
+  var resultS = confirmSpecialCharacters();
+  if (resultS === answeryes) {
+    return yesS
+  }
+  else if (resultS === answerno) {
+    return noS
+  };
+};
 
 function confirmLowercase() {
   var lowercase = prompt("Do you want your password to contain lowercase characters? Please answer 'yes' or 'no'.")
-  if (lowercase === "yes" || lowercase === "YES" || lowercase === "Yes") {
-    return lowercase
+  lowercase.toLowerCase()
+  if (lowercase === "yes") {
+    return answeryes;
   }
-  else {
-    return
+  else if (lowercase === "no") {
+    return answerno
   }
 }
 
 function confirmUppercase() {
   var uppercase = prompt("Do you want your password to contain uppercase characters? Please answer 'yes' or 'no'.")
-  if (uppercase === "yes" || lowercase === "YES" || lowercase === "Yes") {
-    return uppercase
+  uppercase.toLowerCase()
+  if (uppercase === "yes") {
+    return answeryes
   }
-  else {
-    return
+  else if (uppercase === "no") {
+    return answerno
   }
 }
 
 function confirmNumeric() {
   var numeric = prompt("Do you want your password to contain numeric characters? Please answer 'yes' or 'no'.")
-  if (numeric === "yes" || lowercase === "YES" || lowercase === "Yes") {
-    return numeric
+  numeric.toLowerCase()
+  if (numeric === "yes") {
+    return answeryes
   }
-  else {
-    return
+  else if (numeric === "no") {
+    return answerno
   }
 }
 
 function confirmSpecialCharacters() {
   var special = prompt("Do you want your password to contain special characters? Please answer 'yes' or 'no'.")
-  if (special === "yes" || lowercase === "YES" || lowercase === "Yes") {
-    return special
+  special.toLowerCase()
+  if (special === "yes") {
+    return answeryes
   }
-  else {
-    return
+  else if (special === "no") {
+    return answerno
   }
 }
 
-function validateUserChoice() {
-  
-}
+// function validateUserChoice() {
+
+// }
